b138cc20ee5585ba69d307eb1ce53fbf3ae86d76
morioprog <morio.prog@gmail.com>
2022-08-28 23:33:01 +0900

    refactor: move `link` tags to `_document.tsx`

---
+++ nextjs/pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
